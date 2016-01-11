86e372e76839afc3f67003b4d1ee6d94a66f00b2
warrior75 <waryor75@gmail.com>
2016-01-11 10:33:42 +0100

    verif connexion

--- inscription.php
+++ inscription.php
@@ -13,8 +13,7 @@
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <style>
             body {
-                padding-top: 50px;
-                padding-bottom: 20px;
+                    padding-bottom: 20px;
             }
         </style>
         <link rel="stylesheet" href="css/bootstrap-theme.min.css">
