28980c1a761aedb73bdb2050ae0e43bc7dd38bd2
warrior75 <waryor75@gmail.com>
2016-01-11 16:41:52 +0100

    gestion recherche avancee

--- catalogue.php
+++ catalogue.php
@@ -1,5 +1,57 @@
+<?php
+    session_start();
+    require_once(__DIR__.'/config/db.php');
+    $pagesGames= 0;
 
+    // if (!isset($_SESSION['user'])) {
+    //     header('Location :index.php');
+    //     die();
+    // }
+            if(!isset($_POST['action'])) {
+                // Afficher le catalogue entier
+                $query = $pdo->query('SELECT count(id) as total  FROM games');
+                $nbGames = $query->fetch();
+                $totalGames = $nbGames['total'];
+                // 2. trouver la focntion qui arrondi une décimale à son entier supérieur et utiliser la fonction
+                $limiteGames = 5;
+                $pagesGames = ceil($totalGames / $limiteGames); // valeur 5
+                // 6. récupérer la variable page envoyé en GET et l'affecter à $pageGames
+                    if (isset($_GET['page'])&& ($_GET['page']>0) && ($_GET['page']<=$pagesGames)) {
+                    $pageActive = $_GET['page'];
+                    } else {
+                    $pageActive = 1;
+                    }
+                    $debut = ($pageActive-1)*$limiteGames;
+                    // 4. COnstruire la requête SQL pour récupérer les 5 premiers Games
+                    /*echo '<pre>';
+                    print_r($contenu_page);
+                    echo '</pre>';*/
+                    $query = $pdo->prepare('SELECT * FROM games LIMIT :limit  OFFSET :offset');
+                    $query -> bindValue(':limit',$limiteGames,PDO::PARAM_INT);
+                    $query -> bindValue(':offset',$debut,PDO::PARAM_INT);
+                    $query -> execute();
+                    $resultGame = $query->fetchAll();
+            } else {
 
+                $search = $_POST['search'];
+                print_r($_POST);
+                $plateform_id = $_POST['plateform'];
+                if ($plateform_id != 0) {                         
+                    $query = $pdo -> prepare('SELECT * FROM games WHERE title LIKE :title AND plateform_id =:plateform_id');
+                    $query -> bindValue(':title','%'.$search.'%',PDO::PARAM_STR);
+                    $query -> bindValue(':plateform_id',$plateform_id,PDO::PARAM_INT);
+                    $query -> execute();
+                    $resultGame = $query -> fetchAll();
+                } else {
+                    $query = $pdo -> prepare('SELECT * FROM games WHERE title LIKE :title');
+                    $query -> bindValue(':title','%'.$search.'%',PDO::PARAM_STR);
+                    $query -> execute();
+                    $resultGame = $query -> fetchAll();
+                }
+            }
+            
+    
+?>
 <!doctype html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
@@ -50,16 +102,16 @@
                 <form method="POST" action="#"> 
                   <div class="form-group">
                     <label for="search">Rechercher :</label>
-                    <input type="text" class="form-control" id="search" placeholder="search">
+                    <input type="text" name="search" class="form-control" placeholder="search">
                 </div>
                 <div class="form-group">
                   <label for="plateform">Plateform :</label>
-                  <select class="form-control" id="plateform">
-                    <option selected>TOUS</option>
-                    <option>PC</option>
-                    <option>XBOX ONE</option>
-                    <option>PS4</option>
-                    <option>Wii U</option>
+                  <select class="form-control" id="plateform" name="plateform">
+                    <option selected value="0">TOUS</option>
+                    <option value="1">PC</option>
+                    <option value="2">XBOX ONE</option>
+                    <option value="3">PS4</option>
+                    <option value="4">Wii U</option>
                 </select>
             </div>
 
@@ -73,43 +125,54 @@
 
   </div>
   <div class="col-md-9" id="catalogue">
-        <div class="fiche">
-            <img src="http://image.jeuxvideo.com/images-sm/jaquettes/00034710/jaquette-grand-theft-auto-v-pc-cover-avant-g-1415122060.jpg" width="150" height="212" alt="GTA V">
-            <h4>GTA V</h4>
-            <p>Yooo Bro</p>
-            <button type="submit" name="action" class="btn btn-success">Louer</button>
-        </div class="fiche">
-        <div class="fiche">
-            <img src="http://image.jeuxvideo.com/images-sm/jaquettes/00034710/jaquette-grand-theft-auto-v-pc-cover-avant-g-1415122060.jpg" width="150" height="212" alt="GTA V">
-            <h4>GTA V</h4>
-            <p>Yooo Bro</p>
-            <button type="submit" name="action" class="btn btn-success">Louer</button>
-        </div>
-          <div class="fiche">
-            <img src="http://image.jeuxvideo.com/images-sm/jaquettes/00034710/jaquette-grand-theft-auto-v-pc-cover-avant-g-1415122060.jpg" width="150" height="212" alt="GTA V">
-            <h4>GTA V</h4>
-            <p>Yooo Bro</p>
-            <button type="submit" name="action" class="btn btn-success">Louer</button>
-        </div>
-          <div class="fiche">
-            <img src="http://image.jeuxvideo.com/images-sm/jaquettes/00034710/jaquette-grand-theft-auto-v-pc-cover-avant-g-1415122060.jpg" width="150" height="212" alt="GTA V">
-            <h4>GTA V</h4>
-            <p>Yooo Bro</p>
-            <button type="submit" name="action" class="btn btn-success">Louer</button>
-        </div>
-
-       <div>
-    <ul class="pagination">
-              <li><a href="#">Previous</a></li>
-              <li><a href="#">1</a></li>
-              <li><a href="#">2</a></li>
-              <li><a href="#">3</a></li>
-              <li><a href="#">4</a></li>
-              <li><a href="#">5</a></li>
-            <li><a href="#">Next</a></li>
+            <?php if (!empty($resultGame)): ?>
+                
             
-        </ul>
-        </div>
+            <!-- 2. Dynamiser avec php -->
+                <?php foreach($resultGame as $key => $game): ?>
+                    <div class="fiche">
+                    <img src="<?php echo $game['img']; ?>" height="170" width="120">
+                    <h5><?php echo $game['title'] ?></h5>
+                    <!-- <p><?php echo $game['description'] ?></p> -->
+                    <a href="#"><button type="submit" name="action" class="btn btn-success">Louer</button></a>
+                    </div>
+                <?php endforeach; ?>
+            <?php else : ?>
+                <div class="alert alert-info">
+                    <p>Aucun jeu ne correspond à votre recherche</p>
+                </div>
+            <?php endif; ?>
+            <?php if ($pagesGames): ?>
+                  
+        <!-- pagination du bas de la page -->
+                    <div>                    
+                      <ul class="pagination">
+                      <!-- 8. mettre la pagination suivant et prédedent -->
+                            <li>
+                                <a href="catalogue.php?page= <?php echo $pageActive-1; ?> " aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                </a>
+                            </li>
+
+                      <!-- 3. Construire la pagination pour n nombres de pages $pagesGames -->
+                        
+                    
+                            <?php for ($i=1; $i <= $pagesGames ; $i++): ?>
+                           <li class="<?php if ($i == $pageActive ){echo 'active';}?>"><a href="catalogue.php?page=<?php echo $i; ?>"> <?php echo $i; ?></a></li>
+                             <?php endfor; ?>
+                            <li>
+                                <!-- le lien pointe vers le numéro de la page courante +1 récupéré en GET -->
+                                <a href="catalogue.php?page= <?php echo $pageActive+1; ?> " aria-label="Next"> 
+                                    <span aria-hidden="true">&raquo;</span>
+                                </a>
+                            </li>
+                      </ul>
+                    </div>
+            <?php endif ?>
+
+       
+       
+        
 
         </div>
     </div>
