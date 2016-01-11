4dddd55d3d23b5acd10875d60d496b55e412e552
bilel95 <meslem.bellal@gmail.com>
2016-01-11 16:46:15 +0100

    mot de passe oublie

--- connexion.php
+++ connexion.php
@@ -118,7 +118,7 @@ if (isset($_POST['action'])) {
 			  
 			  <button type="submit" name="action" class="btn btn-primary">Valider</button>
                <div class="form-group">
-                            <p class="help-block"><a href="forgotPassword.php">Mot de passe ?</a></p>
+                    <p class="help-block"><a href="forgotPassword.php">Mot de passe oublié?</a></p>
                 </div>
 			</form>
 			
