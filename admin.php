c5950187b19be3b46b277d352a2765ebddc2bc79
Nadia78 <environnement.eau@gmail.com>
2016-01-13 12:24:06 +0100

    fix admin

--- admin.php
+++ admin.php
@@ -1,3 +1,5 @@
+
+
 <?php
 	session_start();
 
@@ -23,20 +25,55 @@
 	$totalUser = $resultCount['total'];// affiche dans le page admin
 	
 ?>
-<h2>Statistiques</h2>
+<!DOCTYPE html>
+<html>
+   <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<p>Le site contient <?php echo $totalUser ?>utilisateur(s).</p>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <style>
+            body {
+               
+                padding-bottom: 20px;
+            }
+        </style>
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
 
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+  <header>
+      <nav class="navbar navbar-inversed">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <a class="navbar-brand" href="index.php">GAMELOC</a>
+            </div>
 
-<p>Cette session est visible que pour les administrateurs</p>
-
-<h1>Localisation des utilisateurs</h1>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="inscription.php">Inscription</a></li>
+                <li><a href="connexion.php">Connexion</a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+    <h2>Statistiques</h2>
+    <p>Le site contient <?php echo $totalUser ?>utilisateur(s).</p>
+    <p>Cette session est visible que pour les administrateurs</p>
+    <h1>Localisation des utilisateurs</h1>
 
     <style type="text/css">
      
       #map { height: 200px; }
     </style>
-  </head>
+
+  </header>
   <body>
     <div id="map"></div>
     <script type="text/javascript">
@@ -65,9 +102,20 @@ function initMap() {
     <script async defer
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyApFHyhOE1lniNGNo0yrkthO-wEUp4OOzM&callback=initMap">
     </script>
+
+
 <h1>Les derniers jeux ajoutés par les inscrits</h1>
 
+    <footer>
+        <p>&copy; Mohand, Nadia , Bilel</p>
+      </footer>
+    <!-- /container -->        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/main.js"></script>
 
   </body>
 </html>
